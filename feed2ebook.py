85e193b32f18ac50e3cabb82e6f5d8cb8e743de2
Johannes Bleier <johannes.bleier@gmx.de>
2019-06-04 11:09:31 +0200

    Writes metadata and title page to ebook
    Converting first to markdown allows the script to add metadata to the
    output format while prepending them to the output string.
    This leads to a better formatted epub format, which can be read from a
    Pocketbook reader without errors.
    
    During this commit the first generated html text is better formatted now
    and the title is removed from the html text, because it is added to the
    resulting document via metadata and written on the title page.

--- feed2ebook.py
+++ feed2ebook.py
@@ -8,19 +8,25 @@ if len(sys.argv) is not 2:
     print "usage: feed2ebook.py <url>"
     exit()
 
+ascii = lambda x: x.encode('ascii', 'xmlcharrefreplace')
+metadata = lambda title, author, addition: '% ' + title + '\n% ' + author + '\n% ' + addition + '\n\n'
+
 d = feedparser.parse(sys.argv[1])
 feed = d.feed.title
 for item in d.entries:
-    ascii = lambda x: x.encode('ascii', 'xmlcharrefreplace')
     title = ascii(item.title)
-    output = "---\n"
-    output = "title: '" + feed + "'\n"
-    output = "---\n"
-    output += "<h2>" + title + "</h2>\n"
+    output = "<html><body>\n"
     for content in item.content:
         output += ascii(content.value) + "\n"
     else:
         output += ascii(item.description) + "\n"
-    date = datetime.fromtimestamp(mktime(item.published_parsed)).strftime('%Y-%m-%dT%H:%M:%S')
-    filename = feed + ' ' + date + '.epub'
-    pypandoc.convert_text(output, 'epub3', format='html', outputfile=filename)
+    output += "</body></html>\n"
+
+    # bypass over markdown to add title information without writing extra metadata file
+    md_output = pypandoc.convert_text(output, 'markdown', format='html')
+    display_date = item.published
+    md_output = metadata(title, feed, display_date) + md_output
+
+    name_date = datetime.fromtimestamp(mktime(item.published_parsed)).strftime('%Y-%m-%dT%H:%M:%S')
+    filename = feed + ' ' + name_date + '.epub'
+    pypandoc.convert_text(md_output, 'epub3', format='markdown', outputfile=filename)
